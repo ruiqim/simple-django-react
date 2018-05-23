293e34b1165b136fbc7612d0926ba7d0c59bffa8
ruiqi mao <iamruiqi@gmail.com>
2018-05-23 12:30:17 -0400

    finished simple django-drf-react with testing

--- backend/frontend/cypress/integration/app_spec.js
+++ backend/frontend/cypress/integration/app_spec.js
@@ -31,4 +31,9 @@ describe("Django REST framework / React quickstart app", () => {
     cy.get("form").submit();
   })
 
+  it("should be able to see the table", () => {
+    cy.visit("/");
+    cy.get("tr").contains(`${lead.name}${lead.email}${lead.message}`);
+  });
+
 })
